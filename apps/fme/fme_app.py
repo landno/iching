743877b8fab44cc149ed937f3915880971fa9ae5
Yantao <yantao@arxandt.com>
2020-04-11 23:23:11 +0800

    build fme app code framework

--- apps/fme/fme_app.py
+++ apps/fme/fme_app.py
@@ -1,6 +1,7 @@
 #
 import random
 import numpy as np
+from apps.rxgb.rxgb_ds import RxgbDs
 
 class FmeApp(object):
     def __init__(self):
@@ -8,17 +9,29 @@ class FmeApp(object):
     
     def startup(self):
         print('金融市场强化学习环境 v0.0.1')
+        # test program
         mode = FmeApp.MODE_MAIN
         if FmeApp.MODE_MAIN == mode:
             self.run_main()
         elif FmeApp.MODE_TEST == mode:
             self.run_test()
+        # main program
+        fme_ds = RxgbDs()
+        # 
+
+    def train(self):
+        # train phase
+        pass
+
+    def run(self):
+        # 
+        pass
 
     MODE_MAIN = 100
     MODE_TEST = 101
 
     def run_main(self):
-        print('运行主程序')
+        pass
 
     def run_test(sefl):
         print('运行测试程序')
