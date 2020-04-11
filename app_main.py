ba200997948d20cc228aba965614805019b0f4c4
Yantao <yantao@arxandt.com>
2020-04-11 19:03:54 +0800

    add dataset class

--- app_main.py
+++ app_main.py
@@ -4,6 +4,7 @@ from apps.ogml.ogml_app import OgmlApp
 from apps.tp.tp_app import TpApp
 from apps.rxgb.rxgb_app import RxgbApp
 from apps.asml.asml_app import AsmlApp
+from apps.fme.fme_app import FmeApp
 
 def norm_batch_tasks(batch_vals, task_num):
     arrs = []
@@ -29,7 +30,8 @@ def main():
     #app = OgmlApp()
     #app = TpApp()
     #app = RxgbApp()
-    app = AsmlApp()
+    #app = AsmlApp()
+    app = FmeApp()
     app.startup()
 
 if '__main__' == __name__:
--- apps/fme/fme_app.py
+++ apps/fme/fme_app.py
@@ -1,71 +1,24 @@
 #
 import random
 import numpy as np
-from apps.fme.asdk_ds import AsdkDs
-from apps.fme.rxgb_strategy import RxgbStrategy
-from apps.fme.asdk_agent import AsdkAgent
 
 class FmeApp(object):
     def __init__(self):
         self.name = 'apps.fme.FmeApp'
     
     def startup(self):
-        print('基于XGBoost强化学习算法的量化交易系统')
-        mode = 3
-        if 1 == mode:
-            self.run_create_ds()
-        elif 2 == mode:
-            self.run_asdk_strategy_train()
-        elif 3 == mode:
-            self.run_asdk_agent()
-        elif 4 == mode:
-            self.run_temp()
-        elif 5 == mode:
-            self.run_t001()
+        print('金融市场强化学习环境 v0.0.1')
+        mode = FmeApp.MODE_MAIN
+        if FmeApp.MODE_MAIN == mode:
+            self.run_main()
+        elif FmeApp.MODE_TEST == mode:
+            self.run_test()
 
-    def run_create_ds(self):
-        stock_code = '601006'
-        start_date = '2006-08-01'
-        end_date = '2007-12-31'
-        ds = AsdkDs()
-        X_train, y_train, X_mu, X_std = ds.create_ds(stock_code, start_date, end_date)
-        test_start_date = '2008-01-01'
-        test_end_date = '2008-12-31'
-        X_test, y_test, _, _ = ds.create_ds(stock_code, start_date, end_date, X_mu, X_std)
-        print('X_test:{0}; \r\n{1}'.format(X_test.shape, X_test))
+    MODE_MAIN = 100
+    MODE_TEST = 101
 
-    def run_asdk_strategy_train(self):
-        stock_code = '601006'
-        start_date = '2006-08-01'
-        end_date = '2007-09-27'
-        ds = AsdkDs()
-        X_train, y_train, X_mu, X_std = ds.create_ds(stock_code, start_date, end_date)
-        test_start_date = '2007-04-26'
-        test_end_date = '2007-05-31'
-        X_test, y_test, _, _ = ds.create_ds(stock_code, test_start_date, test_end_date, X_mu, X_std)
-        strategy = RxgbStrategy()
-        strategy.train(X_train, y_train, X_test, y_test)
-        print(X_test[0, :])
+    def run_main(self):
+        print('运行主程序')
 
-    def run_asdk_agent(self):
-        agent = AsdkAgent()
-        agent.startup()
-
-    def run_temp(self):
-        stock_code = '601006'
-        start_date = '2007-04-26'
-        end_date = '2007-12-31'
-        ds = AsdkbDs()
-        date_list = ds.get_date_list(stock_code, start_date, end_date)
-        print(date_list)
-
-    def run_t001(self):
-        print('t001')
-        y_exp = np.zeros((5, 3))
-        y_exp[3][1] = 1
-        y_exp[3][0] = 1
-        print(y_exp)
-        for ii in range(y_exp.shape[1]):
-            if y_exp[3][ii] == 0:
-                print('choose {0};'.format(ii))
-                break
+    def run_test(sefl):
+        print('运行测试程序')
