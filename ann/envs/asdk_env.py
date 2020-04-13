b25f5592ab4dde007146ce8a0b2f8d801e00069e
yt7589 <yt7589@qq.com>
2020-04-13 22:16:35 +0800

    refactor AsdkEnv._take_action to call _do_hold_action, _do_sell_action, _do_buy_action

--- ann/envs/asdk_env.py
+++ ann/envs/asdk_env.py
@@ -81,66 +81,77 @@ class AsdkEnv(FmeEnv):
         quant = 0
         cost = 0.0
         if 0 == action_type:
-            self.position = np.append(self.position, [self.position[-1]])
-            self.balance = np.append(self.balance, [self.balance[-1]])
-            price = self.ds[self.current_step][3 + (self.lookback_window_size - 1)*5]
-            net_worth = self.balance[-1] + self.position[-1] * price
-            net_worth = int(net_worth * 100) / 100
-            self.net_worth = np.append(self.net_worth, [net_worth])
-            self.trades = np.append(self.trades, [{
-                'date': '', 
-                'open': self.ds[self.current_step][0 + current_idx],
-                'high': self.ds[self.current_step][1 + current_idx],
-                'low': self.ds[self.current_step][2 + current_idx],
-                'close': self.ds[self.current_step][3 + current_idx],
-                'volume': self.ds[self.current_step][4 + current_idx],
-                'type': 0, 'quant': 0, 'position': self.position[-1],
-                'balance': self.balance[-1], 'net_worth': self.net_worth[-1]
-            }])
+            quant, amount, cost = self._do_hold_action(current_idx, price)
         elif 1 == action_type:
-            quant = self.position[-1]
-            amount = quant * price
-            cost = CnaStock.sell_stock_cost(amount)
-            balance = self.balance[-1] + amount - cost
-            balance = int(balance * 100) / 100.0
-            net_worth = balance
-            self.position = np.append(self.position, [0])
-            self.balance = np.append(self.balance, [balance])
-            self.net_worth = np.append(self.net_worth, [net_worth])
-            self.trades = np.append(self.trades, [{
-                'date': '', 
-                'open': self.ds[self.current_step][0 + current_idx],
-                'high': self.ds[self.current_step][1 + current_idx],
-                'low': self.ds[self.current_step][2 + current_idx],
-                'close': self.ds[self.current_step][3 + current_idx],
-                'volume': self.ds[self.current_step][4 + current_idx],
-                'type': 1, 'quant': quant, 'position': self.position[-1],
-                'balance': self.balance[-1], 'net_worth': self.net_worth[-1]
-            }])
-        elif 2 == action_type:            
-            quant = int(self.balance[-1] / price)
+            quant, amount, cost = self._do_sell_action(current_idx, price)
+        elif 2 == action_type:
+            quant, amount, cost = self._do_buy_action(current_idx, price)
+        return {'price': price, 'quant': quant, 'cost': cost}
+
+    def _do_hold_action(self, current_idx, price):
+        self.position = np.append(self.position, [self.position[-1]])
+        self.balance = np.append(self.balance, [self.balance[-1]])
+        net_worth = self.balance[-1] + self.position[-1] * price
+        net_worth = int(net_worth * 100) / 100
+        self.net_worth = np.append(self.net_worth, [net_worth])
+        self.trades = np.append(self.trades, [{
+            'date': '', 
+            'open': self.ds[self.current_step][0 + current_idx],
+            'high': self.ds[self.current_step][1 + current_idx],
+            'low': self.ds[self.current_step][2 + current_idx],
+            'close': self.ds[self.current_step][3 + current_idx],
+            'volume': self.ds[self.current_step][4 + current_idx],
+            'type': 0, 'quant': 0, 'position': self.position[-1],
+            'balance': self.balance[-1], 'net_worth': self.net_worth[-1]
+        }])
+        return 0, 0.0, 0.0
+
+    def _do_sell_action(self, current_idx, price):
+        quant = self.position[-1]
+        amount = quant * price
+        cost = CnaStock.sell_stock_cost(amount)
+        balance = self.balance[-1] + amount - cost
+        balance = int(balance * 100) / 100.0
+        net_worth = balance
+        self.position = np.append(self.position, [0])
+        self.balance = np.append(self.balance, [balance])
+        self.net_worth = np.append(self.net_worth, [net_worth])
+        self.trades = np.append(self.trades, [{
+            'date': '', 
+            'open': self.ds[self.current_step][0 + current_idx],
+            'high': self.ds[self.current_step][1 + current_idx],
+            'low': self.ds[self.current_step][2 + current_idx],
+            'close': self.ds[self.current_step][3 + current_idx],
+            'volume': self.ds[self.current_step][4 + current_idx],
+            'type': 1, 'quant': quant, 'position': self.position[-1],
+            'balance': self.balance[-1], 'net_worth': self.net_worth[-1]
+        }])
+        return quant, amount, cost
+
+    def _do_buy_action(self, current_idx, price):           
+        quant = int(self.balance[-1] / price)
+        amount = quant * price
+        cost = CnaStock.buy_stock_cost(amount)
+        while self.balance[-1] - amount - cost < 0:
+            quant -= 1
             amount = quant * price
             cost = CnaStock.buy_stock_cost(amount)
-            while self.balance[-1] - amount - cost < 0:
-                quant -= 1
-                amount = quant * price
-                cost = CnaStock.buy_stock_cost(amount)
-            balance = self.balance[-1] - amount - cost
-            balance = int(balance * 100) / 100.0
-            position = self.position[-1] + quant
-            net_worth = balance + amount
-            net_worth = int(net_worth * 100) / 100.0
-            self.position = np.append(self.position, [position])
-            self.balance = np.append(self.balance, [balance])
-            self.net_worth = np.append(self.net_worth, [net_worth])
-            self.trades = np.append(self.trades, [{
-                'date': '', 
-                'open': self.ds[self.current_step][0 + current_idx],
-                'high': self.ds[self.current_step][1 + current_idx],
-                'low': self.ds[self.current_step][2 + current_idx],
-                'close': self.ds[self.current_step][3 + current_idx],
-                'volume': self.ds[self.current_step][4 + current_idx],
-                'type': 2, 'quant': quant, 'position': self.position[-1],
-                'balance': self.balance[-1], 'net_worth': self.net_worth[-1]
-            }])
-        return {'price': price, 'quant': quant, 'cost': cost}
+        balance = self.balance[-1] - amount - cost
+        balance = int(balance * 100) / 100.0
+        position = self.position[-1] + quant
+        net_worth = balance + amount
+        net_worth = int(net_worth * 100) / 100.0
+        self.position = np.append(self.position, [position])
+        self.balance = np.append(self.balance, [balance])
+        self.net_worth = np.append(self.net_worth, [net_worth])
+        self.trades = np.append(self.trades, [{
+            'date': '', 
+            'open': self.ds[self.current_step][0 + current_idx],
+            'high': self.ds[self.current_step][1 + current_idx],
+            'low': self.ds[self.current_step][2 + current_idx],
+            'close': self.ds[self.current_step][3 + current_idx],
+            'volume': self.ds[self.current_step][4 + current_idx],
+            'type': 2, 'quant': quant, 'position': self.position[-1],
+            'balance': self.balance[-1], 'net_worth': self.net_worth[-1]
+        }])
+        return quant, amount, cost
