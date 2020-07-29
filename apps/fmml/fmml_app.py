f67ef4426af5c9b10cedd4fe078bd6c6e6d5e46f
yt7589 <yt7589@qq.com>
2020-07-29 14:49:54 +0800

    add a basic building block

--- apps/fmml/fmml_app.py
+++ apps/fmml/fmml_app.py
@@ -1,4 +1,9 @@
 # 金融市场元学习平台
+# 参考资料：
+# arXiv:1905.10437v4: N-BEATS: NEURAL BASIS EXPANSION ANALYSIS FOR 
+#           INTERPRETABLE TIME SERIES FORECASTING
+# arXiv:2002.02887v1: Meta-learning framework with applications to 
+#           zero-shot time-series forecasting
 
 class FmmlApp(object):
     def __init__(self):
---
+++ apps/fmml/nbeats_model.py
@@ -0,0 +1,28 @@
+# 原始论文中的N-BEATS模型类
+import torch
+
+class NbeatsModel(torch.nn.Module):
+    def __init__(self, loopback_window=5, future_horizen=2):
+        '''
+        参数说明：
+            loopback_window 向前看几个时间点
+            future_horizen 预测未来几个时间点
+        '''
+        super(NbeatsModel, self).__init__()
+        self.name = 'apps.fmml.NbeatsModel'
+        self.loopback_window = loopback_window
+        self.future_horizen = future_horizen
+        bocks_per_stack = 2 # 每个stack有2个block
+        # 组成块的全连接层
+        fc_num = 4
+        fc_layers = [8, 8, 8, 8]
+        self.h1 = torch.nn.Linear(loopback_window, fc_layers[0])
+        self.h2 = torch.nn.Linear(fc_layers[0], fc_layers[1])
+        self.h3 = torch.nn.Linear(fc_layers[1], fc_layers[2])
+        self.h4 = torch.nn.Linear(fc_layers[2], fc_layers[3])
+
+    def forward(self, x):
+        h1 = self.h1(x)
+        h2 = self.h2(h1)
+        h3 = self.h3(h2)
+        h4 = self.h4(h3)
