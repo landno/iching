c1ed21d5c48267ca6a86ded495078395a00f8079
yt7589 <yt7589@qq.com>
2020-07-29 15:17:08 +0800

    create basic block class

--- apps/fmml/fmml_app.py
+++ apps/fmml/fmml_app.py
@@ -12,9 +12,10 @@ class FmmlApp(object):
         self.name = 'apps.fmml.FmmlApp'
     
     def startup(self):
-        print('金融市场元学习平台v0.0.2')
+        print('金融市场元学习平台v0.0.3')
         model = NbeatsModel()
         x = torch.tensor([1.0, 2.0, 3.0, 4.0, 5.0])
-        y = model(x)
-        print(y)
+        y_hat, x_hat = model(x)
+        print('y_hat: {0};'.format(y_hat))
+        print('x_hat: {0};'.format(x_hat))
         print('^_^ The End!')
--- apps/fmml/nbeats_model.py
+++ apps/fmml/nbeats_model.py
@@ -10,20 +10,42 @@ class NbeatsModel(torch.nn.Module):
         '''
         super(NbeatsModel, self).__init__()
         self.name = 'apps.fmml.NbeatsModel'
+        self.block = BasicBlock(loopback_window, future_horizen)
+
+    def forward(self, x):
+        y_hat, x_hat = self.block(x)
+        return y_hat, x_hat
+
+class BasicBlock(torch.nn.Module):
+    def __init__(self, loopback_window=5, future_horizen=2):
+        '''
+        参数说明：
+            loopback_window 向前看几个时间点
+            future_horizen 预测未来几个时间点
+        '''
+        super(BasicBlock, self).__init__()
         self.loopback_window = loopback_window
         self.future_horizen = future_horizen
-        bocks_per_stack = 2 # 每个stack有2个block
+        theta_f_num = 5
+        theta_b_num = 5
         # 组成块的全连接层
-        fc_num = 4
         fc_layers = [8, 8, 8, 8]
         self.h1 = torch.nn.Linear(loopback_window, fc_layers[0])
         self.h2 = torch.nn.Linear(fc_layers[0], fc_layers[1])
         self.h3 = torch.nn.Linear(fc_layers[1], fc_layers[2])
         self.h4 = torch.nn.Linear(fc_layers[2], fc_layers[3])
+        self.theta_f = torch.nn.Linear(fc_layers[3], theta_f_num, bias=False)
+        self.y_hat = torch.nn.Linear(theta_f_num, future_horizen)
+        self.theta_b = torch.nn.Linear(fc_layers[3], theta_b_num, bias=False)
+        self.x_hat = torch.nn.Linear(theta_b_num, self.loopback_window)
 
     def forward(self, x):
         h1 = self.h1(x)
         h2 = self.h2(h1)
         h3 = self.h3(h2)
         h4 = self.h4(h3)
-        return h4
+        theta_f = self.theta_f(h4)
+        y_hat = self.y_hat(theta_f)
+        theta_b = self.theta_b(h4)
+        x_hat = self.x_hat(theta_b)
+        return y_hat, x_hat
