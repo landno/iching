74686bb08ef3ea054f155364ec490c066e98cf62
yt7589 <yt7589@qq.com>
2020-07-29 17:50:39 +0800

    test BlockStack

--- apps/fmml/fmml_app.py
+++ apps/fmml/fmml_app.py
@@ -12,7 +12,7 @@ class FmmlApp(object):
         self.name = 'apps.fmml.FmmlApp'
     
     def startup(self):
-        print('金融市场元学习平台v0.0.3')
+        print('金融市场元学习平台v0.0.4')
         model = NbeatsModel()
         x = torch.tensor([1.0, 2.0, 3.0, 4.0, 5.0])
         y_hat, x_hat = model(x)
