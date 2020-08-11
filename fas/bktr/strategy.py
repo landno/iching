06dff8c5d22c226852a47c8189cd16ed916238fc
yt7589 <yt7589@qq.com>
2020-08-11 17:29:16 +0800

    add strategy base class

---
+++ fas/bktr/strategy.py
@@ -0,0 +1,23 @@
+# 策略类基类
+from fas.bktr.order import Order
+
+class Strategy(object):
+    def __init__(self):
+        self.name = 'fas.bktr.Strategy'
+        self.event_send_order = None
+
+    def event_tick(self, market_data):
+        pass
+
+    def event_order(self, order):
+        pass
+
+    def event_position(self, position):
+        pass
+
+    def send_order(self, timestamp, symbol, is_buy, quant):
+        if self.event_send_order is not None:
+            order = Order(timestamp, symbol, quant, is_buy, 
+                        order_type=Order.OT_MARKET_ORDER, 
+                        price=0.0)
+            self.event_send_order(order)
