1898bad58991e78c23cf76dc80705e0e1e7e13f3
yt7589 <yt7589@qq.com>
2020-08-12 16:06:42 +0800

    add BktrEngine construction method

--- fas/bktr/bktr_engine.py
+++ fas/bktr/bktr_engine.py
@@ -1,5 +1,6 @@
 #
-
+import datetime as dt
+import pandas as pd
 from fas.bktr.position import Position
 from fas.bktr.asdk_tick_data import AsdkTickData
 from fas.bktr.market_data import MarketData
@@ -8,8 +9,17 @@ from fas.bktr.order import Order
 
 
 class BktrEngine(object):
-    def __init__(self):
+    def __init__(self, symbol, start_date, end_date):
         self.name = 'fas.bktr.BktrEngine'
+        self.symbol = symbol
+        self.start_date = start_date
+        self.end_date = end_date
+        self.market_data_sources = [] # 所有的市场数据源
+        self.strategy = None 
+        self.unfilled_orders = []
+        self.positions = {}
+        self.current_prices = None
+        self.rpnl, self.npnl = pd.DataFrame(), pd.DataFrame()
 
     def startup(self):
         print('易经量化回测引擎 v0.0.1')
