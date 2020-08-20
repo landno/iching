5af6b1c19c695e6ed4cd301bce0ca81c28ee14e1
yt7589 <yt7589@qq.com>
2020-08-20 11:25:10 +0800

    add security deposit of put option for short side

--- uts/apps/sop/t_option_contract.py
+++ uts/apps/sop/t_option_contract.py
@@ -11,7 +11,7 @@ class TOptionContract(unittest.TestCase):
     def tearDown(cls):
         pass
 
-    def test_calculate_security_deposit(self):
+    def test_calculate_security_deposit1(self):
         ''' 测试认购（看涨）期权保证金计算 '''
         oc = OptionContract()
         oc.exercise_price = 2200
@@ -19,5 +19,15 @@ class TOptionContract(unittest.TestCase):
         oc.option_contract_type = OptionContract.OCT_CALL
         oc.side = OptionContract.SIDE_SHORT
         security_deposit = oc.calculate_security_deposit(2150)
-        print(security_deposit)
         self.assertEqual(security_deposit, 20500, '认购（看涨）期权保证金计算')
+
+    def test_calculate_security_deposit2(self):
+        ''' 测试认沽（看跌）期权保证金计算 '''
+        oc = OptionContract()
+        oc.exercise_price = 2200
+        oc.price = 72
+        close_price = 2150 # 标的收盘价
+        oc.option_contract_type = OptionContract.OCT_PUT
+        oc.side = OptionContract.SIDE_SHORT
+        security_deposit = oc.calculate_security_deposit(close_price)
+        self.assertEqual(security_deposit, 23700, '认沽（看跌）期权保证金计算')
