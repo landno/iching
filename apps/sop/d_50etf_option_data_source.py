4abf400a85029e5decec7e50fba74e6b6610f210
yt7589 <yt7589@qq.com>
2020-08-21 18:39:54 +0800

    fix option code extraction BUG

--- apps/sop/d_50etf_option_data_source.py
+++ apps/sop/d_50etf_option_data_source.py
@@ -1,16 +1,51 @@
 # 50ETF期权日行情数据源
+import numpy as np
 import akshare as ak
 
 class D50etfOptionDataSource(object):
     def __init__(self):
         self.refl = ''
         self.symbol = '50ETF'
+        self.underlying = '510050'
 
     def get_data(self):
         print('获取50ETF期权日行情数据')
+        option_dict = {}
         expire_months = self.get_expire_months()
+        option_codes = self.get_option_codes(expire_months[1])
+        for ocs in option_codes:
+            for option_code in ocs:
+                print('### {0};'.format(option_code))
+            '''
+            option_dict[option_code] = self.get_option_daily_quotation(option_code)
+        dates = []
+        for dt in option_dict[option_codes[0]]:
+            dates.append(dt)
+        for dt in dates:
+            print('### {0}:'.format(dt))
+            '''
 
     def get_expire_months(self):
         ''' 获取合约到期月份 '''
         return ak.option_sina_sse_list(
                     symbol=self.symbol, exchange="null")
+
+    def get_option_codes(self, trade_date):
+        '''
+        获取指定月份的期权合约列表
+        '''
+        return ak.option_sina_sse_codes(trade_date=trade_date,
+                     underlying=self.underlying)
+
+    def get_option_daily_quotation(self, option_code):
+        df = ak.option_sina_sse_daily(code=option_code)
+        X = []
+        dates = df['日期']
+        opens = df['开盘']
+        highs = df['最高']
+        lows = df['最低']
+        closes = df['收盘']
+        volumes = df['成交']
+        for i in range(len(dates)):
+            X.append([dates[i], opens[i], highs[i], lows[i], closes[i], volumes[i]])
+        return np.array(X)
--- uts/apps/sop/t_d_50etf_option_data_source.py
+++ uts/apps/sop/t_d_50etf_option_data_source.py
@@ -15,3 +15,20 @@ class TD50etfOptionDataSource(unittest.TestCase):
         ds = D50etfOptionDataSource()
         expire_months = ds.get_expire_months()
         print(expire_months)
+
+    def test_get_option_codes(self):
+        ds = D50etfOptionDataSource()
+        trade_date = '202009'
+        option_contracts = ds.get_option_codes(trade_date)
+        print(option_contracts)
+
+    def test_get_option_daily_quotation(self):
+        ds = D50etfOptionDataSource()
+        option_code = '10002423'
+        X = ds.get_option_daily_quotation(option_code)
+        print('X: {0};'.format(X.shape))
+        print(X)
+
+    def test_get_data(self):
+        ds = D50etfOptionDataSource()
+        ds.get_data()
