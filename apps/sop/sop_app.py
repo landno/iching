7deddcc78ca5fae768e9335d9bf8bc08f833c178
yt7589 <yt7589@qq.com>
2020-08-21 10:49:49 +0800

    add unit test of D50etfDaataset

--- apps/sop/sop_app.py
+++ apps/sop/sop_app.py
@@ -3,7 +3,6 @@ from apps.sop.option_contract import OptionContract
 from apps.sop.sop_env import SopEnv
 # 仅用于开发测试
 from apps.sop.d_50etf_dataset import D50etfDataset
-import torch.utils.data.dataloader as DataLoader
 
 class SopApp(object):
     def __init__(self):
@@ -11,25 +10,5 @@ class SopApp(object):
 
     def startup(self, args={}):
         print('股票期权平台 v0.0.5')
-        i_debug = 1
-        if 1 == i_debug:
-            self.exp_main()
-            return
         env = SopEnv()
         env.startup(args={})
-
-    def exp_main(self):
-        print('测试程序')
-        mode = 1
-        if 1 == mode:
-            self.exp001()
-
-    def exp001(self):
-        ds = D50etfDataset()
-        dataloader = DataLoader.DataLoader(ds, batch_size= 2, 
-                    shuffle = True, num_workers= 4)
-        for idx, (X, y) in enumerate(dataloader):
-            print('{0}: {1} => {2}; {3};'.format(idx, X, y, type(y)))
-        print('数量：{0};'.format(ds.__len__()))
-        X, y = ds.__getitem__(3)
-        print('样本：{0} => {1};'.format(X, y))
---
+++ uts/apps/sop/t_d_50etf_dataset.py
@@ -0,0 +1,23 @@
+# D50etfDataset测试类
+import unittest
+import torch.utils.data.dataloader as DataLoader
+from apps.sop.d_50etf_dataset import D50etfDataset
+
+class TD50etfDataset(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        pass
+
+    @classmethod
+    def tearDown(cls):
+        pass
+
+    def test_getitem(self):
+        ds = D50etfDataset()
+        dataloader = DataLoader.DataLoader(ds, batch_size= 2, 
+                    shuffle = True, num_workers= 4)
+        for idx, (X, y) in enumerate(dataloader):
+            print('{0}: {1} => {2}; {3};'.format(idx, X, y, type(y)))
+        print('数量：{0};'.format(ds.__len__()))
+        X, y = ds.__getitem__(3)
+        print('样本：{0} => {1};'.format(X, y))
